2fbec45a8d7d533481661ac581f8ba9be9a312ec
saniasajid69 <saniasajid69@gmail.com>
2024-04-27 09:47:04 +0500

    Add Words-Count

---
+++ Words Counter/index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.cyanBright("\n \t\t Sania Sajid - Word Counter"));
+console.log("=".repeat(60))
+
+let answer = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: "input",
+      message: "Enter a Sentence",  
+    }
+]);
+
+let words = answer.sentence.trim().split(" ");
+
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words;"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
